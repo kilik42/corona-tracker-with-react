c626c473376b941e938b3d30f45de89183026881
kilik42 <marvinevins@yahoo.com>
2020-05-03 02:23:25 -0500

    working on cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import {Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
+
 
 class Cards extends Component {
     //console.log(props);
     render(props) {
         return (
-            <div>
+            <div className={styles.container}>
                 <h1>Cards</h1>
             </div>
         );
